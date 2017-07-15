9edef6ce9a9a306e40535c08d3a989bac84af420
Frank Carrasco <carrasco.frank10@gmail.com>
2017-07-15 15:14:43 -0300

    gravando dados no Database do firebase.

--- app/src/main/java/com/android/firebaseapp/firebaseapp/MainActivity.java
+++ app/src/main/java/com/android/firebaseapp/firebaseapp/MainActivity.java
@@ -3,11 +3,37 @@ package com.android.firebaseapp.firebaseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    //recuperando a instancia do Database
+    private DatabaseReference firebaseReferencia = FirebaseDatabase.getInstance().getReference(); //getREference volta ao no incial do banco
+    private DatabaseReference usuarioReferencia = firebaseReferencia.child("usuarios");
+    private DatabaseReference produtoReferencia = firebaseReferencia.child("produtos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Usuarios usuarios = new Usuarios();
+        usuarios.setNome("Ana Helena");
+        usuarios.setSobrenome("Silva");
+        usuarios.setIdade(25);
+        usuarios.setSexo("Femenino");
+
+        Produtos produtos = new Produtos();
+        produtos.setDescricao("Relogio");
+        produtos.setCor("Preto");
+        produtos.setFabricante("Omega");
+
+
+        //firebaseReferencia.child("pontos").setValue("100"); //adicionando valor ao banco
+        //usuarioReferencia.child("002").child("nome").setValue("Juliana Sampaio");
+        usuarioReferencia.child("003").setValue(usuarios);
+        produtoReferencia.child("001").setValue(produtos);
+
     }
 }
---
+++ app/src/main/java/com/android/firebaseapp/firebaseapp/Produtos.java
@@ -0,0 +1,41 @@
+package com.android.firebaseapp.firebaseapp;
+
+/**
+ * Created by Frank on 15/07/2017.
+ */
+
+public class Produtos {
+
+    private String descricao;
+    private String cor;
+    private String fabricante;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public Produtos() {
+
+
+    }
+}
